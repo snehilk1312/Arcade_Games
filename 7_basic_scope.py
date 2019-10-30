9851bb75d6326f7501ea92ade2de570e86131c2b
snehil <44499005+snehilk1312@users.noreply.github.com>
2019-10-30 23:17:35 +0530

    Create 7_basic_scope.py
    This was to resolve some basic confusion related to scope of variable.

---
+++ 7_basic_scope.py
@@ -0,0 +1,26 @@
+def rough():
+    x=2
+    print(x)
+    print(z)
+    # z=z+1
+    # print(z)
+    print('________________-')
+    print(new.x)
+    new.x+=1
+    print(new.x)
+
+def new(y):
+    x=10
+    #print(new.x)
+    print('lola')
+
+
+new.x = 3
+print(x)
+print(type(new.x))
+x=1
+y=rough
+# print(type(y))
+new(y)
+z=1
+rough()
